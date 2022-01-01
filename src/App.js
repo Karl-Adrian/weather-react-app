ded4fa3a3678717ba9e8e852ab2db1259171ee66
Karl Adrian <spidermanady36@gmail.com>
2022-01-01 13:18:47 +0300

    add date

--- src/App.js
+++ src/App.js
@@ -4,12 +4,29 @@ const api = {
 };
 
 function App() {
+  const dateBuilder =(d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri","Sat"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." />
         </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        
+
       </main>
     </div>
   );
